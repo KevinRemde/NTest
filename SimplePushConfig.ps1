5911b52785c641d58754d0af79dbb9e9006ce02f
Kevin Remde <kevin@remde.net>
2016-09-23 15:52:29 -0500

    DSC

---
+++ SimplePushConfig.ps1
@@ -0,0 +1,23 @@
+﻿Configuration SimplePushConfig {
+    Node "localhost" {
+
+#        WindowsFeature WebServer {
+#            Ensure="Present"
+#            Name="Web-Server"
+#        }
+
+        File MyFile {
+            DestinationPath = "C:\MyFile.txt"
+            Contents = "Hello World"
+        }        
+        
+        File MyOtherFile {
+            DestinationPath = "C:\MyOtherFile.txt"
+            Contents = "Goodbye World"
+        }
+    }
+}
+
+SimplePushConfig
+Start-DscConfiguration -Path .\SimplePushConfig -Wait -Verbose -ComputerName localhost
+# Start-DscConfiguration -Path .\SimplePushConfig -Wait -Verbose -ComputerName localhost -Force
